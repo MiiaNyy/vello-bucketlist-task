bb7b83ef1662f4a70a4115b1bb10a2c0d5622c79
MiiaNyy <mi.nyyssonen@gmail.com>
2022-10-19 12:53:04 +0300

    first mockup for the list item table done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <p>{{ bucketList }}</p>
-  <button @click="changeStatus">Change Antarctica status</button>
+  <div class="dialog">
+    <div class="dialog__close">x</div>
+    <div class="container">
+      <h1>Bucket list</h1>
+      <ListItems :list="bucketList"/>
+      <div class="dialog__footer"></div>
+    </div>
+  </div>
+
 </template>
 
 <script>
 
 import { ref } from "vue";
 import BucketListAPI from "@/services/BucketListAPI";
+import ListItems from "@/components/ListItems";
 
 export default {
   setup () {
@@ -16,7 +24,7 @@ export default {
       try {
         const response = await BucketListAPI.getBucketList();
         console.log( response );
-        bucketList.value = response.data;
+        bucketList.value = response.data.data;
 
       } catch ( e ) {
         console.error( e );
@@ -30,17 +38,60 @@ export default {
     }
   },
   name : 'App',
-  components : {},
+  components : { ListItems },
 }
 </script>
 
 <style>
+:root {
+  warning-clr: #FF5093;
+  dark-theme-clr: #002844;
+  light-theme-clr: #67889F;
+  light-activity-clr: #F5F5F5;
+  dark-activivity-clr: #CCD7DF;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+
+
+/*---TYPOGRAPHY---*/
+
+h1, h2, h3, h4, h5, h6, p {
+  margin-top: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.dialog {
+  width: 100%;
+  max-width: 700px;
+
+  padding: 0.5em 1em;
+  border: 1px solid pink;
+}
+
+.dialog__close {
+  text-align: right;
+}
+
+
+
 </style>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dialog__list-container">
+
+    <table>
+      <tr>
+        <th class="borders">Done</th>
+        <th class="borders"></th>
+        <th class="borders">Do before age</th>
+        <th class="borders"></th>
+      </tr>
+      <tr v-for="item in list" v-bind:key="item.uuid">
+        <td>{{ item.done }}</td>
+        <td>{{ item.description }}</td>
+        <td>{{ item['do_before'] }}</td>
+        <td>
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </td>
+      </tr>
+
+    </table>
+    <ul>
+      <li v-for="listItem in list" v-bind:key="listItem.uuid">
+        {{ listItem }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props : ['list'],
+  name : "ListItems"
+}
+</script>
+
+<style scoped>
+
+.borders {
+  border: 2px solid pink;
+}
+
+</style>
